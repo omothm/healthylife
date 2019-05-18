7dc5854630afed5e4ca81af841b78170ed71751b
Omar Othman <omar@omothm.com>
2019-05-19 01:18:31 +0300

    Adds ID field and setters

--- app/src/main/java/com/omothm/healthylife/models/Bmi.java
+++ app/src/main/java/com/omothm/healthylife/models/Bmi.java
@@ -1,10 +1,12 @@
 package com.omothm.healthylife.models;
 
 import com.omothm.healthylife.db.SQLiteDate;
+import org.jetbrains.annotations.NotNull;
 
 public class Bmi {
 
   private SQLiteDate date;
+  private long id;
   private float value;
 
   public Bmi(final SQLiteDate date, final float value) {
@@ -16,10 +18,27 @@ public class Bmi {
     return date;
   }
 
+  public void setDate(final SQLiteDate date) {
+    this.date = date;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
   public float getValue() {
     return value;
   }
 
+  public void setValue(float value) {
+    this.value = value;
+  }
+
+  @NotNull
   @Override
   public String toString() {
     return date.toString() + ": " + value;
