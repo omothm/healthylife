efb9dd7e5af815607a5065713879a22f5164bf48
Omar Othman <omar@omothm.com>
2019-05-19 10:13:21 +0300

    Adds chart and test layout

--- app/src/main/java/com/omothm/healthylife/activities/BmiActivity.java
+++ app/src/main/java/com/omothm/healthylife/activities/BmiActivity.java
@@ -1,29 +1,102 @@
 package com.omothm.healthylife.activities;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.omothm.healthylife.R;
+import com.omothm.healthylife.db.BmiSource;
+import com.omothm.healthylife.db.DbHelper;
+import com.omothm.healthylife.models.Bmi;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BmiActivity extends AppCompatActivity {
+
+  private BmiSource bmiSource;
+  private LineChart chart;
+  private SQLiteDatabase database;
+  private DbHelper dbHelper;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_bmi);
 
+    // Setup back button
     final ActionBar ab = getSupportActionBar();
     if (ab != null) {
       ab.setDisplayHomeAsUpEnabled(true);
     }
+
+    dbHelper = new DbHelper(this);
+
+    chart = findViewById(R.id.chart);
+
   }
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
+    // End if back button is pressed
     if (item.getItemId() == android.R.id.home) {
       finish();
     }
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    // Close database
+    dbHelper.close();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // Open database
+    database = dbHelper.open();
+
+    bmiSource = new BmiSource(database);
+    final List<Bmi> bmis = bmiSource.getAll();
+    LineData data = setupData(bmis);
+    ChartHelper.setupChart(chart, data, new ValueFormatter() {
+      @Override
+      public String getFormattedValue(float value) {
+        return bmis.get((int) value).getDate().toString();
+      }
+    });
+  }
+
+  private LineData setupData(final List<Bmi> bmis) {
+
+    ArrayList<Entry> values = new ArrayList<>();
+
+    for (int i = 0; i < bmis.size(); i++) {
+      float val = bmis.get(i).getValue();
+      values.add(new Entry(i, val));
+    }
+
+    final int color = ContextCompat.getColor(this, R.color.colorPrimary);
+
+    // create a dataset and give it a type
+    LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+    set1.setLineWidth(1.75f);
+    set1.setCircleRadius(5f);
+    set1.setCircleHoleRadius(2.5f);
+    set1.setColor(color);
+    set1.setCircleColor(color);
+    set1.setHighLightColor(color);
+    set1.setDrawValues(false);
+
+    // create a data object with the data sets
+    return new LineData(set1);
   }
 }
---
+++ app/src/main/java/com/omothm/healthylife/activities/ChartHelper.java
@@ -0,0 +1,58 @@
+package com.omothm.healthylife.activities;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.XAxis.XAxisPosition;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.components.YAxis.YAxisLabelPosition;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+public class ChartHelper {
+
+  public static void setupChart(LineChart chart, LineData data, ValueFormatter formatter) {
+    // no description text
+    chart.getDescription().setEnabled(false);
+
+    chart.setDrawGridBackground(true);
+
+    // enable touch gestures
+    chart.setTouchEnabled(true);
+
+    // enable scaling and dragging
+    chart.setDragEnabled(true);
+    chart.setScaleEnabled(true);
+
+    // if disabled, scaling can be done on x- and y-axis separately
+    chart.setPinchZoom(false);
+
+    // set custom chart offsets (automatic offset calculation is hereby disabled)
+    chart.setViewPortOffsets(10, 0, 10, 0);
+
+    // add data
+    chart.setData(data);
+
+    chart.getAxisLeft().setEnabled(false);
+    chart.getAxisLeft().setSpaceTop(40);
+    chart.getAxisLeft().setSpaceBottom(40);
+    chart.getAxisRight().setEnabled(false);
+
+    XAxis xAxis = chart.getXAxis();
+    xAxis.setPosition(XAxisPosition.BOTTOM_INSIDE);
+    xAxis.setEnabled(true);
+    xAxis.setDrawAxisLine(true);
+    xAxis.setDrawGridLines(true);
+    xAxis.setDrawLabels(true);
+    xAxis.setValueFormatter(formatter);
+    YAxis yAxis = chart.getAxisRight();
+    yAxis.setPosition(YAxisLabelPosition.INSIDE_CHART);
+    yAxis.setEnabled(true);
+    yAxis.setDrawAxisLine(true);
+    yAxis.setDrawGridLines(true);
+    yAxis.setDrawLabels(true);
+
+    // animate calls invalidate()...
+    chart.animateX(1500);
+  }
+}
